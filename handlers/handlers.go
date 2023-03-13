c82a5aff4b7ccb55aa162549589b663176fcf293
Rob Ranf <rob@emiyaconsulting.com>
2023-03-13 15:58:01 -0700

    adding base, home, and nav templates

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,12 +2,40 @@ package handlers
 
 import (
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hell, Madison"))
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/base.gohtml",
+		"./ui/html/partials/nav.gohtml",
+		"./ui/html/pages/home.gohtml",
+	}
+
+	// files... is a variadic parameter meaning it can refer to any number of parameters, in that the
+	// slice of file paths in files can be of any length. The ... is the "variadic operator" and
+	// works similar to the functionality of the ... spread operator in JavaScript. We can see in the doc
+	// that ParseFiles is a variadic function in that it has in its function signature (filenames ...string)
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
 
 func SnippetView(w http.ResponseWriter, r *http.Request) {
