d60b85562393c565cb39ddff84299bd0fa139640
Rob Ranf <rob@emiyaconsulting.com>
2023-04-26 21:04:30 -0700

    sql driver, db setup and snippet model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,39 +1,67 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/rlr524/snippetboxv2/internal/models"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Config struct {
 	addr      string
 	staticDir string
 	env       string
+	dsn       string
 }
 
 type Application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
 	cfg      Config
+	snippets *models.SnippetModel
 }
 
 func main() {
 	var cfg Config
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbPass := os.Getenv("DB_PASS")
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.StringVar(&cfg.env, "env", "production", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.dsn, "dsn", fmt.Sprintf("web:%s@tcp(lancer:3306)/snippetbox?parseTime=true", dbPass), "MySQL data source")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
 
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			errorLog.Print(err)
+		}
+	}(db)
+
 	app := &Application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -43,8 +71,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", cfg.addr)
-	err := srv.ListenAndServe()
-	errorLog.Fatal(err)
+	e := srv.ListenAndServe()
+	errorLog.Fatal(e)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
 
 func NeuteredFileSystem(next http.Handler) http.Handler {
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/rlr524/snippetboxv2
 
 go 1.20
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
