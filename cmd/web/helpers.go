424be5347a2e1f03584d0405ba0b221f64bd9b3f
Rob Ranf <rob@emiyaconsulting.com>
2023-07-13 17:53:55 -0700

    render function

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -11,7 +11,10 @@ import (
 // trace from the current goroutine and appends it to the log message.
 func (app *Application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
-	app.errorLog.Output(2, trace)
+	e := app.errorLog.Output(2, trace)
+	if e != nil {
+		return
+	}
 
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 }
@@ -26,4 +29,24 @@ func (app *Application) clientError(w http.ResponseWriter, status int) {
 // The notFound helper is a convenience wrapper around the 404 Not Found client response.
 func (app *Application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
+}
+
+func (app *Application) render(w http.ResponseWriter, status int, page string, data *TemplateData) {
+	// Retrieve the appropriate template set from the cache based on the page name. If no entry exists in the cache
+	// with the provided name, create a new error and call the serverError() helper method.
+	ts, ok := app.templateCache[page]
+	if !ok {
+		err := fmt.Errorf("the template %s does not exist", page)
+		app.serverError(w, err)
+		return
+	}
+
+	// Write out the provided HTTP status code.
+	w.WriteHeader(status)
+
+	// Execute the template set and write the response body.
+	err := ts.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		app.serverError(w, err)
+	}
 }
