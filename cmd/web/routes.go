a828a17dcf7628419df1093873e08d182bd17b1c
Rob Ranf <rob@emiyaconsulting.com>
2023-07-27 07:45:04 -0700

    adding headers middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,7 +6,8 @@ func (app *Application) Routes() *http.ServeMux {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	//fileServer := http.FileServer(http.Dir(app.cfg.staticDir))
+	// StripPrefix is added here as middleware to remove /static from the /static/ routes and
+	// hand them over to the NeuteredFileSystem() method to disallow traversing of the static directory.
 	mux.Handle("/static/", http.StripPrefix("/static", NeuteredFileSystem(fileServer)))
 
 	mux.HandleFunc("/", app.home)
